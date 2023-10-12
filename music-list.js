d9c1186327657427bdda6df5516f9199daeadf6d
PixtionVS <76754245+PixtionVS@users.noreply.github.com>
2023-10-11 21:30:07 -0600

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Gostyle 2023 sped up",
+    artist: "evan aloe, evan aloe xo",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Moto Moto",
+    artist: "evan aloe xo",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "E-Girl Bounce",
+    artist: "Shiki-TMNS, PiNKII",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "b33n.tuff",
+    artist: "IDY bran",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Simp With Me",
+    artist: "YungLex, Watergun Collective",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Nikes",
+    artist: "Nimsterr, kid toni",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Bali",
+    artist: "LostOnRodeoDrive",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "pushin B",
+    artist: "evan aloe xo, evan aloe",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Young Girl A - one day After Another Remix",
+    artist: "Silamota, sasanomaly",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Rose",
+    artist: "YungLex, Lil Boom, Ciscaux",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
